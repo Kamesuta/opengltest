a2af4575e71db7ede2a2c416a8b12d978007858a
Kamesuta <kamesuta@gmail.com>
2022-05-11 02:10:30 +0900

    vlc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod support;
 
+extern crate vlc;
+
+use std::sync::mpsc::channel;
+use vlc::{Instance, Media, MediaPlayer, EventType, State};
+use vlc::Event as VlcEvent;
+
 use glutin::event::{Event, WindowEvent};
 use glutin::event_loop::{ControlFlow, EventLoop};
 use glutin::window::WindowBuilder;
@@ -35,4 +41,42 @@ fn main() {
             _ => (),
         }
     });
+
+
+    let args: Vec<String> = std::env::args().collect();
+
+    let path = match args.get(1) {
+        Some(s) => s,
+        None => {
+            println!("Usage: cli_audio_player path_to_a_media_file");
+            return;
+        }
+    };
+    let instance = Instance::new().unwrap();
+    
+    let md = Media::new_path(&instance, path).unwrap();    
+    let mdp = MediaPlayer::new(&instance).unwrap();
+    
+    let (tx, rx) = channel::<()>();
+    
+    let em = md.event_manager();
+    let _ = em.attach(EventType::MediaStateChanged, move |e, _| {
+        match e {
+            VlcEvent::MediaStateChanged(s) => {
+                println!("State : {:?}", s);
+                if s == State::Ended || s == State::Error {
+                    tx.send(()).unwrap();
+                }
+            },
+            _ => (),
+        }
+    });
+    
+    mdp.set_media(&md);
+    
+    // Start playing
+    mdp.play().unwrap();
+    
+    // Wait for end state
+    rx.recv().unwrap();    
 }
